417c3b0a3c35442e49f280c4f1957c00fc4c15ea
Rafael Morais <rafaelm.morais@hotmail.com>
2024-09-18 12:47:33 -0300

    aula 03

--- cypress/support/pages/home.page.js
+++ cypress/support/pages/home.page.js
@@ -7,6 +7,12 @@ export const homePage = {
     openSearchProduct(){
         cy.get('[data-testid="search-products"]').click()
     },
+    searchProduct(product){
+        cy.get('[data-testid="searchInput"]').type(product)
+    },
+    products(){
+        return cy.get('[data-testid="browse-product-list"] [data-testid="productDetails"]')
+    },
     openCategoriesFilter(){
         cy.get('[data-testid="Category"]').click()
     },
