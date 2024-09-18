6eef9bad18105f4391da7b35a80f9866f4888b77
Rafael Morais <rafaelm.morais@hotmail.com>
2024-09-18 11:38:46 -0300

    Aula 01

--- cypress/e2e/intercept.cy.js
+++ cypress/e2e/intercept.cy.js
@@ -4,11 +4,11 @@ const { email, senha } = require('../fixtures/data.json')
 const { homePage } = require("../support/pages/home.page.js");
 
 describe('Teste de Autenticação', () => {
-
   beforeEach(() => {
     cy.login(email, senha)
   })
 
+  // Mostrar as categorias visiveis
   it('categories should be visible', () => {
     cy.intercept('GET', '**/public/getCategories', { fixture: 'categories.json' }).as('getCategories')
     homePage.openSearchProduct()
@@ -16,6 +16,7 @@ describe('Teste de Autenticação', () => {
     homePage.categories().should('have.length.greaterThan', 1)
   })
 
+  // Categorias devem apresentar vazias
   it('categories should be empty', () => {
     cy.intercept('GET', '**/public/getCategories', { fixture: 'noCategories.json' }).as('getCategoriesEmpty')
     homePage.openSearchProduct()
@@ -23,6 +24,7 @@ describe('Teste de Autenticação', () => {
     homePage.categories().should('have.length', 1)
   })
 
+  // Status code deve ser 500 ( ERROR )
   it('categories should be empty with error', () => {
     cy.intercept('GET', '**/public/getCategories', { statusCode: 500 }).as('getCategoriesError')
     homePage.openSearchProduct()
